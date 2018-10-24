82b25ef7923bf12fbcfa62a8b04facb441593ef6
ThisIsPan <42142442+thisisapan@users.noreply.github.com>
2018-10-25 00:23:57 +0900

    create New Project

---
+++ src/main/java/com/demo/sample/SampleDemo/controller/Sample.java
@@ -0,0 +1,19 @@
+package com.demo.sample.SampleDemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/")
+@Controller
+public class Sample {
+
+    @GetMapping
+    public String initDisp(Model model) {
+
+
+
+        return "sample";
+    }
+}
